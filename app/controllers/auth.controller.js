144fa1d510e8e1cbfa5a1e47563a0c027a783abd
jonathanweinstein <jon.weinstein@uppercampus.com>
2022-11-18 18:03:49 -0500

    testing pool vs sequelize

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -8,115 +8,135 @@ const Op = db.Sequelize.Op;
 
 var jwt = require("jsonwebtoken")
 const { user } = require("../models");
+const Pool = require('pg').Pool;
+require('dotenv').config();
 
-exports.login = async (req, res) => {
-  console.log('at login');
-  console.log(' ');
-  if (db) {
-    console.log('yes db')
-    if (db.sequelize) {
-      console.log('yes sequelize')
-      try {
-        await db.sequelize.authenticate();
-        console.log('sequelize authenticated')
-      } catch (err) {
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
 
-        console.log('cant auth sequelize');
-      }
-
-      // if (req.body.credential) {
-      //   console.log('credential');
-      //   console.log(' ');
-      //   if (req.body.type && req.body.type === "bearer") {
-      //     const googleUrl = "https://www.googleapis.com/oauth2/v1/tokeninfo?bearer_token=" + req.body.credential
-      //     // console.log(googleUrl);
-
-
-
-      //     // .then(() => {
-      //     //   console.log(' ');
-      //     //   console.log('Connection has been established successfully.');
-      //     //   console.log(' ');
-      //     // axios({
-      //     //   url: googleUrl,
-      //     //   method: "POST",
-      //     // }).then(async (response) => {
-      //     //   console.log('received a google profile');
-      //     //   console.log(response.data);
-      //     //   console.log(' ');
-      //     //   const user = await User.findOne({
-      //     //     where: {
-      //     //       email: response.data.email
-      //     //     },
-      //     //     raw: true
-      //     //   }).catch(err => {
-      //     //     console.log('in err');
-      //     //     console.log(err);
-      //     //     res.status(500).send({ message: err.message });
-      //     //   });
-      //     //   if (!user) {
-      //     //     console.log('no user');
-      //     //     console.log(' ');
-      //     //     const roles = [];
-      //     //     // SET ROLES HERE
-      //     //     User.create({
-      //     //       email: response.data.email
-      //     //     })
-      //     //       .then(user => {
-      //     //         console.log('got user');
-      //     //         //NEED TO SIGN IN HERE
-      //     //         // console.log(user);
-      //     //         console.log(' ');
-      //     //         res.status(200).send(user);
-      //     //         // if (roles) {
-      //     //         //   Role.findAll({
-      //     //         //     where: {
-      //     //         //       name: {
-      //     //         //         [Op.or]: req.body.roles
-      //     //         //       }
-      //     //         //     }
-      //     //         //   }).then(roles => {
-      //     //         //     user.setRoles(roles).then(() => {
-      //     //         //       res.status(200).send(user);
-      //     //         //     });
-      //     //         //   });
-      //     //         // } else {
-      //     //         //   // user role = 1
-      //     //         //   user.setRoles([1]).then(() => {
-      //     //         //     res.status(200).send(user);
-      //     //         //   });
-      //     //         // }
-      //     //       })
-      //     //   } else {
-      //     //     console.log('already a user');
-      //     //     // console.log(user);
-      //     //     // console.log(' ');
-      //     //     //NEED TO SIGN IN HERE;
-      //     //     res.status(200).send(user);
-
-      //     //   }
-      //     // }).catch(err => {
-      //     //   console.log('in error 2');
-      //     //   console.log(err);
-      //     //   res.status(500).send({ message: err.message });
-      //     // });
-      //     // })
-      //     // .catch(err => {
-      //     //   console.log('unable to connect', err);
-      //     //   // console.error('Unable to connect to the database:', err);
-      //     // });
-      //   }
-      // }
+exports.login = async (request, response) => {
+  console.log('at login')
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      console.log('error');
+      throw error;
     }
-    else {
-      console.log('no sequelize')
-    }
-  }
-  else {
-    console.log('no db object')
-  }
-
+    response.status(200).json(results.rows);
+  });
 };
+// exports.login = async (req, res) => {
+//   console.log('at login');
+//   console.log(' ');
+//   if (db) {
+//     console.log('yes db')
+//     if (db.sequelize) {
+//       console.log('yes sequelize')
+//       try {
+//         await db.sequelize.authenticate();
+//         console.log('sequelize authenticated')
+//       } catch (err) {
+
+//         console.log('cant auth sequelize');
+//       }
+
+//       // if (req.body.credential) {
+//       //   console.log('credential');
+//       //   console.log(' ');
+//       //   if (req.body.type && req.body.type === "bearer") {
+//       //     const googleUrl = "https://www.googleapis.com/oauth2/v1/tokeninfo?bearer_token=" + req.body.credential
+//       //     // console.log(googleUrl);
+
+
+
+//       //     // .then(() => {
+//       //     //   console.log(' ');
+//       //     //   console.log('Connection has been established successfully.');
+//       //     //   console.log(' ');
+//       //     // axios({
+//       //     //   url: googleUrl,
+//       //     //   method: "POST",
+//       //     // }).then(async (response) => {
+//       //     //   console.log('received a google profile');
+//       //     //   console.log(response.data);
+//       //     //   console.log(' ');
+//       //     //   const user = await User.findOne({
+//       //     //     where: {
+//       //     //       email: response.data.email
+//       //     //     },
+//       //     //     raw: true
+//       //     //   }).catch(err => {
+//       //     //     console.log('in err');
+//       //     //     console.log(err);
+//       //     //     res.status(500).send({ message: err.message });
+//       //     //   });
+//       //     //   if (!user) {
+//       //     //     console.log('no user');
+//       //     //     console.log(' ');
+//       //     //     const roles = [];
+//       //     //     // SET ROLES HERE
+//       //     //     User.create({
+//       //     //       email: response.data.email
+//       //     //     })
+//       //     //       .then(user => {
+//       //     //         console.log('got user');
+//       //     //         //NEED TO SIGN IN HERE
+//       //     //         // console.log(user);
+//       //     //         console.log(' ');
+//       //     //         res.status(200).send(user);
+//       //     //         // if (roles) {
+//       //     //         //   Role.findAll({
+//       //     //         //     where: {
+//       //     //         //       name: {
+//       //     //         //         [Op.or]: req.body.roles
+//       //     //         //       }
+//       //     //         //     }
+//       //     //         //   }).then(roles => {
+//       //     //         //     user.setRoles(roles).then(() => {
+//       //     //         //       res.status(200).send(user);
+//       //     //         //     });
+//       //     //         //   });
+//       //     //         // } else {
+//       //     //         //   // user role = 1
+//       //     //         //   user.setRoles([1]).then(() => {
+//       //     //         //     res.status(200).send(user);
+//       //     //         //   });
+//       //     //         // }
+//       //     //       })
+//       //     //   } else {
+//       //     //     console.log('already a user');
+//       //     //     // console.log(user);
+//       //     //     // console.log(' ');
+//       //     //     //NEED TO SIGN IN HERE;
+//       //     //     res.status(200).send(user);
+
+//       //     //   }
+//       //     // }).catch(err => {
+//       //     //   console.log('in error 2');
+//       //     //   console.log(err);
+//       //     //   res.status(500).send({ message: err.message });
+//       //     // });
+//       //     // })
+//       //     // .catch(err => {
+//       //     //   console.log('unable to connect', err);
+//       //     //   // console.error('Unable to connect to the database:', err);
+//       //     // });
+//       //   }
+//       // }
+//     }
+//     else {
+//       console.log('no sequelize')
+//     }
+//   }
+//   else {
+//     console.log('no db object')
+//   }
+
+// };
 
 exports.signin = (req, res) => {
   User.findOne({
