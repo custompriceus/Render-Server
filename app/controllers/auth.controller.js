c41e23a7f5dad4ad9e9ed1031a57339edf3c364d
jonathanweinstein <jon.weinstein@uppercampus.com>
2022-11-18 16:52:04 -0500

    trying to check sequelize connection

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -13,80 +13,85 @@ exports.login = async (req, res) => {
   console.log('at login');
   console.log(' ');
   if (req.body.credential) {
+    console.log('credential');
+    console.log(' ');
     if (req.body.type && req.body.type === "bearer") {
       const googleUrl = "https://www.googleapis.com/oauth2/v1/tokeninfo?bearer_token=" + req.body.credential
+      // console.log(googleUrl);
       await db.sequelize
         .authenticate()
         .then(() => {
+          console.log(' ');
           console.log('Connection has been established successfully.');
-          axios({
-            url: googleUrl,
-            method: "POST",
-          }).then(async (response) => {
-            console.log('received a google profile');
-            console.log(response.data);
-            console.log(' ');
-            const user = await User.findOne({
-              where: {
-                email: response.data.email
-              },
-              raw: true
-            }).catch(err => {
-              console.log('in err');
-              console.log(err);
-              res.status(500).send({ message: err.message });
-            });
-            if (!user) {
-              console.log('no user');
-              console.log(' ');
-              const roles = [];
-              // SET ROLES HERE
-              User.create({
-                email: response.data.email
-              })
-                .then(user => {
-                  console.log('got user');
-                  //NEED TO SIGN IN HERE
-                  // console.log(user);
-                  console.log(' ');
-                  res.status(200).send(user);
-                  // if (roles) {
-                  //   Role.findAll({
-                  //     where: {
-                  //       name: {
-                  //         [Op.or]: req.body.roles
-                  //       }
-                  //     }
-                  //   }).then(roles => {
-                  //     user.setRoles(roles).then(() => {
-                  //       res.status(200).send(user);
-                  //     });
-                  //   });
-                  // } else {
-                  //   // user role = 1
-                  //   user.setRoles([1]).then(() => {
-                  //     res.status(200).send(user);
-                  //   });
-                  // }
-                })
-            } else {
-              console.log('already a user');
-              // console.log(user);
-              // console.log(' ');
-              //NEED TO SIGN IN HERE;
-              res.status(200).send(user);
+          console.log(' ');
+          // axios({
+          //   url: googleUrl,
+          //   method: "POST",
+          // }).then(async (response) => {
+          //   console.log('received a google profile');
+          //   console.log(response.data);
+          //   console.log(' ');
+          //   const user = await User.findOne({
+          //     where: {
+          //       email: response.data.email
+          //     },
+          //     raw: true
+          //   }).catch(err => {
+          //     console.log('in err');
+          //     console.log(err);
+          //     res.status(500).send({ message: err.message });
+          //   });
+          //   if (!user) {
+          //     console.log('no user');
+          //     console.log(' ');
+          //     const roles = [];
+          //     // SET ROLES HERE
+          //     User.create({
+          //       email: response.data.email
+          //     })
+          //       .then(user => {
+          //         console.log('got user');
+          //         //NEED TO SIGN IN HERE
+          //         // console.log(user);
+          //         console.log(' ');
+          //         res.status(200).send(user);
+          //         // if (roles) {
+          //         //   Role.findAll({
+          //         //     where: {
+          //         //       name: {
+          //         //         [Op.or]: req.body.roles
+          //         //       }
+          //         //     }
+          //         //   }).then(roles => {
+          //         //     user.setRoles(roles).then(() => {
+          //         //       res.status(200).send(user);
+          //         //     });
+          //         //   });
+          //         // } else {
+          //         //   // user role = 1
+          //         //   user.setRoles([1]).then(() => {
+          //         //     res.status(200).send(user);
+          //         //   });
+          //         // }
+          //       })
+          //   } else {
+          //     console.log('already a user');
+          //     // console.log(user);
+          //     // console.log(' ');
+          //     //NEED TO SIGN IN HERE;
+          //     res.status(200).send(user);
 
-            }
-          }).catch(err => {
-            console.log('in error 2');
-            console.log(err);
-            res.status(500).send({ message: err.message });
-          });
+          //   }
+          // }).catch(err => {
+          //   console.log('in error 2');
+          //   console.log(err);
+          //   res.status(500).send({ message: err.message });
+          // });
         })
         .catch(err => {
-          console.error('Unable to connect to the database:', err);
+          console.log('unable to connect', err);
+          // console.error('Unable to connect to the database:', err);
         });
-
     }
   }
 };
