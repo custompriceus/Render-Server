015aa4445a0325906e96f260f4d90e10e12b173f
jonathanweinstein <jon.weinstein@uppercampus.com>
2022-11-19 17:43:44 -0500

    added drop and resync tables to a route

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(cors({
 const db = require("./app/models");
 const Role = db.role;
 
-db.sequelize.sync({ force: true }).then(() => {
-  console.log('Drop and Resync Database with { force: true }');
-  initial()
-})
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log('Drop and Resync Database with { force: true }');
+//   initial()
+// })
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
