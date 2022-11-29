db113b0e0538061742f8db3b33fd53b8a4073760
jonathanweinstein <jon.weinstein@uppercampus.com>
2022-11-28 21:43:56 -0500

    trying to track down prod login issue

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -7,7 +7,7 @@ const { dbService } = require("../services");
 
 const verifyGoogleToken = async (token) => {
   console.log(' ');
-  console.log('verify google token ,')
+  console.log('at verify google token ,')
   const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
   const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
@@ -23,9 +23,11 @@ const verifyGoogleToken = async (token) => {
 }
 
 const verifyBearerToken = async (token) => {
-  const googleUrl = "https://www.googleapis.com/oauth2/v3/tokeninfo?"
   console.log(' ');
-  // console.log('google url ,', googleUrl)
+  console.log('at verify bearer - VERIFY BEARER START')
+  const googleUrl = "https://www.googleapis.com/oauth2/v3/tokeninfo?"
+  console.log('google url, ' + googleUrl);
+
   try {
     const ticket = await axios({
       url: googleUrl,
@@ -39,11 +41,13 @@ const verifyBearerToken = async (token) => {
       // console.log(' ');
       // console.log('ticket ', ticket.data)
       const payload = { sub: ticket.data.sub, email: ticket.data.email }
-      console.log('payload ', payload)
+      console.log(' ');
+      console.log('got a payload at verify bearer - VERIFY BEARER END', payload)
       return { payload: payload };
     }
   } catch (error) {
-    console.log('error at auth controller');
+    console.log(' ');
+    console.log('error at verify bearer token - VERIFY BEARER END');
     console.log(error);
     return { error: "Invalid user detected. Please try again" };
   }
@@ -88,48 +92,61 @@ const getUserByIdAndSignIn = async (id) => {
   return signedInUser;
 }
 
+
+const getToken = async (type, credential) => {
+  console.log(' ');
+  console.log('at get token - GET TOKEN START');
+  const token = type === "bearer" ? await verifyBearerToken(credential) : await verifyGoogleToken(credential);
+
+  if (token) {
+    console.log(' ');
+    console.log('got a token at get token - GET TOKEN END')
+    return token
+  }
+  else {
+    console.log(' ');
+    console.log('no token at get token - GET TOKEN END')
+    return false;
+  }
+
+}
+
 exports.login = async (req, res) => {
   console.log(' ')
-  console.log('at login')
-  const token = req.body.type === "bearer" ? await verifyBearerToken(req.body.credential) : await verifyGoogleToken(req.body.credential);
-  console.log('token ,', token)
-  const googleId = token.payload.sub
-  // console.log(' ')
-  // console.log('google id, ', googleId);
+  console.log('at login - START')
+  await getToken(req.body.type, req.body.credential).then(async (response) => {
+    console.log(' ');
+    console.log('got token ', response);
 
-  try {
-    const userByGoogleId = await dbService.getUserByGoogleId(googleId);
-    console.log(' ')
-    console.log('user by google id, ')
-    if (!userByGoogleId) {
-      console.log(' ')
-      console.log('no google user');
-      const createdUser = await dbService.createUserByGoogleProfile(googleId, token.payload.email)
-      if (!createdUser) {
-        res.status(400).send({
-          message: `Failed to create user with email ${token.payload.email} and google_id ${googleId}`
+    const googleId = response.payload.sub
+
+    await dbService.getUserByGoogleId(googleId).then(async (responseTwo) => {
+      if (!responseTwo) {
+        console.log(' ')
+        console.log('no google user');
+        await dbService.createUserByGoogleProfile(googleId, response.payload.email).then(async (responseThree) => {
+          if (!responseThree) {
+            res.status(400).send({
+              message: `Failed to create user with email ${response.payload.email} and google_id ${googleId}`
+            });
+          }
+          else {
+            console.log(' ')
+            console.log('created a user')
+            const signedInUser = await getUserByIdAndSignIn(responseThree.id);
+            console.log(' ')
+            console.log('signed in user')
+            res.status(200).send(signedInUser);
+          }
         });
       }
       else {
-        console.log(' ')
-        console.log('created a user, ')
-        const signedInUser = await getUserByIdAndSignIn(createdUser.id);
-        console.log(' ')
-        console.log('signed in user, ')
-        res.status(200).send(signedInUser);
+        await getUserByIdAndSignIn(responseTwo.id).then(async (responseFour) => {
+          console.log(' ')
+          console.log('already a user, signed in user')
+          res.status(200).send(responseFour);
+        })
       }
-    }
-    else {
-      const signedInUser = await getUserByIdAndSignIn(userByGoogleId.id);
-      console.log(' ')
-      console.log('already a user, signed in user, ')
-      res.status(200).send(signedInUser);
-    }
-
-  } catch (error) {
-    console.log(error);
-    response.status(500).json({
-      message: error.message || error,
-    });
-  }
+    })
+  })
 };
