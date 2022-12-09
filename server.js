94c3cf85d48d48b3baabb2d84480420ed948de10
jonathanweinstein <jon.weinstein@uppercampus.com>
2022-12-09 01:55:14 -0500

    added shirt pricing

--- server.js
+++ server.js
@@ -14,6 +14,284 @@ app.use(cors({
   origin: '*'
 }));
 
+const shirtPrices = [
+  {
+    quantity: '6-11',
+    colors: 1,
+    price: 3
+  },
+  {
+    quantity: '6-11',
+    colors: 2,
+    price: 4
+  },
+  {
+    quantity: '6-11',
+    colors: 3,
+    price: 5.55
+  },
+  {
+    quantity: '6-11',
+    colors: 4,
+    price: 7.5
+  },
+  {
+    quantity: '6-11',
+    colors: 5,
+    price: 9.0
+  },
+  {
+    quantity: '6-11',
+    colors: 6,
+    price: 11
+  },
+  {
+    quantity: '6-11',
+    colors: 6,
+    price: 11
+  },
+  {
+    quantity: '12-36',
+    colors: 1,
+    price: 1.95
+  },
+  {
+    quantity: '12-36',
+    colors: 2,
+    price: 3.00
+  },
+  {
+    quantity: '12-36',
+    colors: 3,
+    price: 4.1
+  },
+  {
+    quantity: '12-36',
+    colors: 4,
+    price: 5.2
+  },
+  {
+    quantity: '12-36',
+    colors: 5,
+    price: 6.75
+  },
+  {
+    quantity: '12-36',
+    colors: 6,
+    price: 7.5
+  },
+  {
+    quantity: '37-72',
+    colors: 1,
+    price: 1.2
+  },
+  {
+    quantity: '37-72',
+    colors: 2,
+    price: 2.25
+  },
+  {
+    quantity: '37-72',
+    colors: 3,
+    price: 3.2
+  },
+  {
+    quantity: '37-72',
+    colors: 4,
+    price: 3.6
+  },
+  {
+    quantity: '37-72',
+    colors: 5,
+    price: 4.5
+  },
+  {
+    quantity: '37-72',
+    colors: 6,
+    price: 5.5
+  },
+  {
+    quantity: '73-144',
+    colors: 1,
+    price: .9
+  },
+  {
+    quantity: '73-144',
+    colors: 2,
+    price: 1.85
+  },
+  {
+    quantity: '73-144',
+    colors: 3,
+    price: 2.25
+  },
+  {
+    quantity: '73-144',
+    colors: 4,
+    price: 2.6
+  },
+  {
+    quantity: '73-144',
+    colors: 5,
+    price: 3.5
+  },
+  {
+    quantity: '73-144',
+    colors: 6,
+    price: 4.25
+  },
+  {
+    quantity: '145-288',
+    colors: 1,
+    price: .85
+  },
+  {
+    quantity: '145-288',
+    colors: 1,
+    price: 1.3
+  },
+  {
+    quantity: '145-288',
+    colors: 1,
+    price: 1.7
+  },
+  {
+    quantity: '145-288',
+    colors: 1,
+    price: 2.25
+  },
+  {
+    quantity: '145-288',
+    colors: 1,
+    price: 2.75
+  },
+  {
+    quantity: '145-288',
+    colors: 1,
+    price: 3.25
+  },
+  {
+    quantity: '288-499',
+    colors: 1,
+    price: .65
+  },
+  {
+    quantity: '288-499',
+    colors: 2,
+    price: 1.0
+  },
+  {
+    quantity: '288-499',
+    colors: 3,
+    price: 1.35
+  },
+  {
+    quantity: '288-499',
+    colors: 4,
+    price: 1.65
+  },
+  {
+    quantity: '288-499',
+    colors: 5,
+    price: 2.25
+  },
+  {
+    quantity: '288-499',
+    colors: 6,
+    price: 2.75
+  },
+  {
+    quantity: '500-999',
+    colors: 1,
+    price: .55
+  },
+  {
+    quantity: '500-999',
+    colors: 1,
+    price: .85
+  },
+  {
+    quantity: '500-999',
+    colors: 1,
+    price: 1
+  },
+  {
+    quantity: '500-999',
+    colors: 1,
+    price: 1.2
+  },
+  {
+    quantity: '500-999',
+    colors: 1,
+    price: 1.55
+  },
+  {
+    quantity: '500-999',
+    colors: 1,
+    price: 1.8
+  },
+  {
+    quantity: '1000-4999',
+    colors: 1,
+    price: .5
+  },
+  {
+    quantity: '1000-4999',
+    colors: 2,
+    price: .6
+  },
+  {
+    quantity: '1000-4999',
+    colors: 3,
+    price: .7
+  },
+  {
+    quantity: '1000-4999',
+    colors: 4,
+    price: 1
+  },
+  {
+    quantity: '1000-4999',
+    colors: 5,
+    price: 1.25
+  },
+  {
+    quantity: '1000-4999',
+    colors: 6,
+    price: 1.5
+  },
+  {
+    quantity: '5000+',
+    colors: 1,
+    price: .45
+  },
+  {
+    quantity: '5000+',
+    colors: 1,
+    price: .55
+  },
+  {
+    quantity: '5000+',
+    colors: 1,
+    price: .65
+  },
+  {
+    quantity: '5000+',
+    colors: 1,
+    price: .8
+  },
+  {
+    quantity: '5000+',
+    colors: 1,
+    price: 1.1
+  },
+  {
+    quantity: '5000+',
+    colors: 1,
+    price: 1.25
+  }
+]
+
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
@@ -174,6 +452,71 @@ app.post("/createTables", async (req, res) => {
   res.status(201).send('Tables Created');
 
 
+});
+
+createShirtPriceTable = async () => {
+  queryString = `CREATE TABLE IF NOT EXISTS shirtprices(
+    id SERIAL PRIMARY KEY,
+    quantity VARCHAR(255),
+    colors INT,
+    price FLOAT
+    )`
+
+  pool.query(
+    queryString,
+    [],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      return results.rows;
+
+    }
+  );
+}
+
+insertShirtPrices = async () => {
+  shirtPrices.map(async (shirt) => {
+
+    const randInt = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000)
+    try {
+      const res = await pool.query(
+        'INSERT INTO shirtprices (id,quantity,colors,price) VALUES ($1,$2,$3,$4) RETURNING *',
+        [randInt, shirt.quantity, shirt.colors, shirt.price]
+      )
+      return res.rows[0];
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+
+  })
+}
+
+app.post("/createShirtPriceTable", async (req, res) => {
+  createShirtPriceTable();
+  res.status(201).send('Tables Created');
+});
+
+app.post("/insertShirtPrices", async (req, res) => {
+  insertShirtPrices();
+  res.status(201).send('Prices Added');
+});
+
+app.get("/shirtPrices", async (req, res) => {
+  queryString = `SELECT * FROM shirtprices`;
+
+  pool.query(
+    queryString,
+    [],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        throw error;
+      }
+      res.status(201).send(results.rows);
+    }
+  );
 });
 
 // routes
