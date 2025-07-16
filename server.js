6125ad03eca643423dc95189619f30fca1a163ef
custompriceus <custompricelistus@gmail.com>
2025-07-16 22:40:45 +0530

    screencharge

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-    ssl: false // <--- Make sure this is false, not an object!
+    ssl: true // <--- Make sure this is false, not an object!
 });
 
 pool.connect((err, client, done) => {
